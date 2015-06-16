790681c8b2b67745d7ecb81fcb3d4cfdcbe0515e
mirelon <miso@github.ksp.sk>
2015-06-16 22:39:49 +0200

    Proof for modified membrane creation semantics

--- set/work.sh
+++ set/work.sh
@@ -1,4 +1,4 @@
 evince set.pdf &> /dev/null &
 git cola &> /dev/null &
-google-chrome "http://github.com/mirelon/membrane_computing/issues" &> /dev/null
+google-chrome "http://github.com/mirelon/membrane_computing/issues" &> /dev/null &
 subl . &> /dev/null &
